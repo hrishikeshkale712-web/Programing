860744a73879e60566d319ad79fe24e4b0254440
hrishikeshkale712-web <hrishikesh.kale712@gmail.com>
2026-07-01 15:51:41 +0530

    Add files via upload

---
+++ Compititive Programs Of C/DisplayDivisible5.c
@@ -0,0 +1,47 @@
+
+/*
+Accept n Numbers from user and display all such elements which are divisible by 5.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int Display(int Arr[],int iLenght)
+{
+    int i=0;
+    for(i=0;i<=iLenght;i++)
+    {
+        if(Arr[i]%5==0)
+        {
+            printf("%d\t",Arr[i]);
+        }
+    }
+}
+int main()
+{
+    int iSize=0;
+    int iRet=0;
+    int iCnt=0;
+    int *p=NULL;
+    
+    printf("Enter the Number Of elements:");
+    scanf("%d",&iSize);
+
+    p=(int*)malloc(iSize*sizeof(int));
+
+    if(p==NULL)
+    {
+        printf("Unable to Allocate Memory\n");
+        return -1;
+    }
+    printf("Enter %d Elements:\n",iSize);
+
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        printf("Enter Element %d:",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+    Display(p,iSize);
+    free(p);
+
+    return 0;
+}
+
